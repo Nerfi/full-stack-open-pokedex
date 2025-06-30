f35bd480b1c744da74050ca354fe16fd59077a3d
jfparede <jfparede@aicrumit.com>
2025-06-30 13:27:34 +0200

    just for the peace of this

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,10 +27,10 @@ module.exports = {
       "error",
       "unix"
     ],
-    "quotes": [
-      "error",
-      "single"
-    ],
+    // "quotes": [
+    //   "error",
+    //   "single"
+    // ],
     "semi": [
       "error",
       "never"
