99571c93fdeb84f8e0f683cd0dc00581243ad379
jfparede <jfparede@aicrumit.com>
2025-06-30 13:38:58 +0200

    nuevo eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,38 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
+  env: {
+    browser: true,
+    es6: true,
     "jest/globals": true
   },
-  "extends": [
+  extends: [
     "eslint:recommended",
     "plugin:react/recommended"
   ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+    ecmaVersion: 2018,
+    sourceType: "module"
   },
-  "plugins": [
-    "react", "jest"
+  plugins: [
+    "react",
+    "jest"
   ],
-  "rules": {
-    // "indent": [
-    //   "error",
-    //     2
-    //   ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    // "quotes": [
-    //   "error",
-    //   "single"
-    // ],
-    "semi": [
-      "error",
-      "never"
-    ],
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  rules: {
+    // "indent": ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    // "quotes": ["error", "single"],
+    "semi": ["error", "never"],
     "eqeqeq": "error",
     "no-trailing-spaces": "error",
-    "object-curly-spacing": [
-      "error", "always"
-    ],
-    "arrow-spacing": [
-      "error", { "before": true, "after": true }
-    ],
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": ["error", { before: true, after: true }],
     "no-console": "error",
     "react/prop-types": 0
   }
